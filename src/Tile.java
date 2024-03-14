5460fe6e7295475c653c8095709019c31800f037
Dylan <dylanc176@nycstudents.net>
2024-03-14 09:24:46 -0400

    added code to tile class

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,12 @@
 public class Tile {
+    //taken from U9T5 grid game
+    private String symbol;
+
+    public Tile(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
