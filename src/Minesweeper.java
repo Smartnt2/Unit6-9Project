774619fa6b6632074d9a6e5826ecc072535ad795
Dylan <dylanc176@nycstudents.net>
2024-03-14 09:30:29 -0400

    started setupMinefield()

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 public class Minesweeper {
+    private Tile[][] mineField;
     private Scanner scan;
     public Minesweeper() {
         scan = new Scanner(System.in);
+        setupMinefield();
         start();
     }
 
     public void start() {
 
+    }
+
+    public void setupMinefield() {
+        System.out.println("What difficulty would you like?");
+        System.out.print("(e)asy, (m)edium, or (h)ard: ");
+        String difficulty = scan.nextLine();
+        //set board size + num mines depending on difficulty, see minesweeper wikipedia page
     }
 
 }
