6d4d16ded917267647411d581ccde6f33934c8fb
marson <marsonm985@gmail.com>
2024-03-15 08:35:42 -0400

    started setupMinefield()

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class Minesweeper {
     private int totalMines;
@@ -16,23 +17,43 @@ public class Minesweeper {
     public void setupMinefield() {
         System.out.println("What difficulty would you like?");
         System.out.print("(e)asy, (m)edium, or (h)ard: ");
-        String difficulty = scan.nextLine();
+        String difficulty = scan.nextLine().toLowerCase();
         //set board size + num mines depending on difficulty, see minesweeper wikipedia page
         //could use totalMines for generating mines and for showing mines remaining (totalMines - number of flags on grid), can be negative
         if(difficulty.equals("e")) {
             //9x9, 10 mines
             mineField = new Tile[9][9];
             totalMines = 10;
+            for(int r = 0; r < mineField.length; r++) {
+                for(int c = 0; c  < mineField[0].length; c++) {
+                    mineField[r][c] = new Tile("_");
+                }
+            }
+            printMinefield();
 
-        } if(difficulty.equals("m")) {
+        }
+         else if(difficulty.equals("m")) {
             //16x16, 40 mines
             mineField = new Tile[16][16];
             totalMines = 40;
+            for(int r = 0; r < mineField.length; r++) {
+                for(int c = 0; c  < mineField[0].length; c++) {
+                    mineField[r][c] = new Tile("_");
+                }
+            }
 
-        } if(difficulty.equals("h")) {
+
+        }
+        else if(difficulty.equals("h")) {
             //16x30, 99 mines
             mineField = new Tile[16][30];
             totalMines = 99;
+            for(int r = 0; r < mineField.length; r++) {
+                for(int c = 0; c  < mineField[0].length; c++) {
+                    mineField[r][c] = new Tile("_");
+                }
+            }
+            printMinefield();
 
         } else {
             System.out.println("Not an option");
